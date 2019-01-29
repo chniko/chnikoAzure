d87c9d802579a7f1273e09dc7af3d42c4aaa0f2f
chniko <chniko@users.noreply.github.com>
2019-01-29 15:37:43 +0100

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,4 @@
+# Set the home page.
+Set-Content `
+  -Path "C:\\inetpub\\wwwroot\\Default.htm" `
+  -Value "<html><body><h1>Je sais tout</h1><h2><img src="https://csb861c62dc28efx468ax9dc.blob.core.windows.net/bucketchniko/unnamed.jpg" alt="Smiley face"></h2></body></html>"
