631e4497c8aae77886f40f49b40ff9d2bab88d9f
chniko <chniko@users.noreply.github.com>
2019-01-29 15:44:00 +0100

    Update coucouHalim.ps1

--- coucouHalim.ps1
+++ coucouHalim.ps1
@@ -1,5 +1,4 @@
 # Set the home page.
 Set-Content `
   -Path "C:\\inetpub\\wwwroot\\Default.htm" `
-  -Value "<html><body><h2>Salut Halim !</h2><h1>A ce qui parait tu n'invites pas Nico aux pauses ??</h1><i>Pô bien... !</i></body>
-  -Value coucou</html>"
+  -Value "<html><body><h2>Salut Halim !</h2><h1>A ce qui parait tu n'invites pas Nico aux pauses ??</h1><i>Pô bien... !</i><img src="https://csb861c62dc28efx468ax9dc.blob.core.windows.net/bucketchniko/unnamed.jpg" alt="Smiley face"></body></html>"
