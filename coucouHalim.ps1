0d7a3fc7c6528736036c5cb0e8005d50753257a3
chniko <chniko@users.noreply.github.com>
2019-01-29 15:07:26 +0100

    Create coucouHalim.ps1

---
+++ coucouHalim.ps1
@@ -0,0 +1,4 @@
+# Set the home page.
+Set-Content `
+  -Path "C:\\inetpub\\wwwroot\\Default.htm" `
+  -Value "<html><body><h2>Salut Halim !</h2><h1>A ce qui parait tu n'invites pas Nico aux pauses ??</h1><i>Pô bien... !</i></body></html>"
